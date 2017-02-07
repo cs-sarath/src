11671f375b1369566b62b25e6bf9832ee97af4f3
cs-sarath <cs.sarath@gmail.com>
2017-02-07 18:04:17 +0000

    Update HttpServer.java

--- src/src/com/myserver/server/HttpServer.java
+++ src/src/com/myserver/server/HttpServer.java
@@ -6,8 +6,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import com.myserver.start.MyServer;
-
 public class HttpServer implements Server
 {
 
@@ -15,11 +13,12 @@ public class HttpServer implements Server
 	public void start() throws UnknownHostException, IOException 
 	{
 		ServerSocket server = new ServerSocket (5000, 10, InetAddress.getByName("127.0.0.1"));
-		
+		RequestAcceptor requestAcceptor = new RequestAcceptor();
 		while(true) 
 		{
 			Socket connected = server.accept();
-			(new RequestAcceptor(connected)).start();
+			requestAcceptor.execute(connected);
+			System.out.println("accepted Socket"+connected.hashCode());
 		}
 	}
 
