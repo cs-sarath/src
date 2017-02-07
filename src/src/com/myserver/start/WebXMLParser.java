2e8a996a809ac130c05fd1af860321c131b3f844
cs-sarath <cs.sarath@gmail.com>
2017-02-07 18:25:24 +0000

    Update WebXMLParser.java

--- src/src/com/myserver/start/WebXMLParser.java
+++ src/src/com/myserver/start/WebXMLParser.java
@@ -17,73 +17,95 @@ import com.myserver.server.WebApplicationContext;
 class WebXMLParser extends DefaultHandler {
 
 	private String path;
-	
-	private ServletContext servletContext;
-	
+
+	private WebApplicationContext servletContext;
+
 	private ServletWrapper servletWrapper;
-	private boolean servlet = false;
-	private boolean servletName = false;
-	private boolean servletMapping = false;
-	private boolean servletClass = false;
-	private boolean urlPattern = false;
-	
+	private boolean isServlet = false;
+	private boolean isServletName = false;
+	private boolean isServletMapping = false;
+	private boolean isServletClass = false;
+	private boolean isUrlPattern = false;
+	private boolean isDisplayName = false;
+
+	WebXMLParser(File webXML) 
+	{
+		this.path = webXML.toString();
+		this.servletContext = new WebApplicationContext();
+	}
+
 	WebXMLParser(String webAppName, String path)
 	{
 		this.path = path;
 		this.servletContext = new WebApplicationContext(webAppName);
 	}
-	
-	void parse() throws ParserConfigurationException, SAXException, IOException
+
+	WebApplicationContext parse() throws ParserConfigurationException, SAXException, IOException
 	{
 		SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
-        saxParser.parse(new File(path), this);
+		saxParser.parse(new File(path), this);
+		return servletContext;
 	}
-	
+
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes)
 	{
-        if (qName.equalsIgnoreCase("servlet")) {
-        	servlet = true;
-        }
-        else if (qName.equalsIgnoreCase("servlet-name")) {
-            servletName = true;
-        }
-        else if (qName.equalsIgnoreCase("servlet-class")) {
-        	servletClass = true;
-        }
-        else if (qName.equalsIgnoreCase("servlet-mapping")) {
-        	servletMapping = true;
-        }
-        else if (qName.equalsIgnoreCase("url-pattern")) {
-        	urlPattern = true;
-        }
-    }
-	
-    @Override
-    public void endElement(String uri, String localName, String qName) throws SAXException 
-    {
-        if (qName.equalsIgnoreCase("servlet-name")) {
-            servletContext.addServlet(servletWrapper.getServletName(), servletWrapper.getServletClass());
-        }
-    }
-    
-    @Override
-    public void characters(char ch[], 
-       int start, int length) throws SAXException {
-       if (servlet && servletName) {
-    	   this.servletWrapper = new ServletWrapper(new String(ch, start, length));
-    	   servlet = false;
-    	   servletName = false;
-       } else if (servletClass) {
-    	   servletWrapper.setServletClass(new String(ch, start, length));
-    	   servletClass = false;
-       } else if (servletMapping && servletName) {
-          servletMapping = false;
-       } else if (urlPattern) {
-          
-          urlPattern = false;
-       }
-    }
+		if (qName.equalsIgnoreCase("servlet")) {
+			isServlet = true;
+		}
+		else if (qName.equalsIgnoreCase("servlet-name")) {
+			isServletName = true;
+		}
+		else if (qName.equalsIgnoreCase("servlet-class")) {
+			isServletClass = true;
+		}
+		else if (qName.equalsIgnoreCase("servlet-mapping")) {
+			isServletMapping = true;
+		}
+		else if (qName.equalsIgnoreCase("url-pattern")) {
+			isUrlPattern = true;
+		}
+		else if (qName.equalsIgnoreCase("display-name")) {
+			isDisplayName = true;
+		}
+	}
 
-	
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException 
+	{
+		if (qName.equalsIgnoreCase("servlet-name")) {
+			servletContext.addServlet(servletWrapper.getServletName(), servletWrapper.getServletClass());
+		}
+	}
+
+	@Override
+	public void characters(char ch[], 
+			int start, int length) throws SAXException 
+	{
+		if (isServlet && isServletName) 
+		{
+			this.servletWrapper = new ServletWrapper(new String(ch, start, length));
+			isServlet = false;
+			isServletName = false;
+		}
+		else if (isServletClass) 
+		{
+			servletWrapper.setServletClass(new String(ch, start, length));
+			isServletClass = false;
+		}
+		else if (isServletMapping && isServletName)
+		{
+			isServletMapping = false;
+		} 
+		else if (isUrlPattern) 
+		{
+			isUrlPattern = false;
+			servletContext.addServletUrlPattern(new String(ch, start, length), servletWrapper);
+		}
+		else if (isDisplayName) 
+		{
+			isDisplayName = false;
+			servletContext.setWebAppName(new String(ch, start, length));
+		}
+	}
 }
